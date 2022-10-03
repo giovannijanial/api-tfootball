731f5ae3b8c9709eb1675fc25fa4ea73bef65f71
Giovanni Janial <giovannijanial@gmail.com>
2022-10-03 17:44:50 -0300

    feat: draw teams in match

--- src/modules/matches/useCases/drawMatchTeams/DrawMatchTeamsUseCase.ts
+++ src/modules/matches/useCases/drawMatchTeams/DrawMatchTeamsUseCase.ts
@@ -1,12 +1,14 @@
 import { MatchesPlayers, prisma } from "@prisma/client";
 import { prismaClient } from "../../../../database/prismaClient";
 import { AppError } from "../../../../errors/AppError";
+import { shuffleArray } from "../../../players/utils/ShuffleArray";
 
 class DrawMatchTeamsUseCase {
 	async execute(matchId: string) {
 		const matchExist = await prismaClient.match.findUnique({
 			where: { id: matchId },
 			select: {
+				id: true,
 				players: true,
 			},
 		});
@@ -17,27 +19,38 @@ class DrawMatchTeamsUseCase {
 
 		const quantityPlayers = matchExist.players.length;
 		const matchPlayers = matchExist.players;
-		const teamA: MatchesPlayers[] = [];
-		const teamB: MatchesPlayers[] = [];
-
-		for (let i = 0; i < quantityPlayers / 2; i++) {
-			const n = Math.floor(Math.random() * matchPlayers.length);
-			teamA.push(matchPlayers[n]);
-			matchPlayers.splice(n, 1);
-		}
-		matchPlayers.map((player) => teamB.push(player));
 
 		const teamHome = await prismaClient.team.create({ data: {} });
 		const teamOut = await prismaClient.team.create({ data: {} });
 
-		const match = await prismaClient.matchesPlayers.update({
-			where: {
-				id: matchId,
-			},
-			data: { ...updateMatchDTO },
-		});
+		let arrShuffle = shuffleArray(matchPlayers);
+		for (let i = 0; i < quantityPlayers / 2; i++) {
+			const player = arrShuffle.pop();
+			if (player) {
+				await prismaClient.matchesPlayers.update({
+					where: {
+						id: player.id,
+					},
+					data: {
+						teamId: teamHome.id,
+					},
+				});
+			}
+			arrShuffle = shuffleArray(arrShuffle);
+		}
+		for (const player of arrShuffle) {
+			const updated = await prismaClient.matchesPlayers.update({
+				where: {
+					id: player.id,
+				},
+				data: {
+					teamId: teamOut.id,
+				},
+			});
+			console.log(updated);
+		}
 
-		return teamA;
+		return { teamHome, teamOut };
 	}
 }
 
